692de125d8bd084537890a3904eb9fb9b8e89095
Warrenn Enslin <warrennenslin@Warrenns-MacBook-Pro.local>
2019-08-01 12:47:18 +0200

    updated rollup

--- packages/agsm-store/rollup.config.js
+++ packages/agsm-store/rollup.config.js
@@ -1,24 +1,82 @@
 import pkg from './package.json'
 import typescript from 'rollup-plugin-typescript2'
-import dts from 'rollup-plugin-dts'
+import nodeResolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 
-const externalPackages = Object.keys(pkg.dependencies)
+const externalPackages = [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
+]
 
-export default [{
-        input: './src/index.d.ts',
-        plugins: [dts()],
-        output: { file: pkg.types }
-    },
+export default [ // CommonJS
     {
-        input: './src/index.ts',
+        input: 'src/index.ts',
+        output: { file: 'lib/agsm-store.js', format: 'cjs', indent: false },
         external: externalPackages,
+        plugins: [typescript()]
+    },
+
+    // ES
+    {
+        input: 'src/index.ts',
+        output: { file: 'es/agsm-store.js', format: 'es', indent: false },
+        external: externalPackages,
+        plugins: [typescript()]
+    },
+
+    // ES for Browsers
+    {
+        input: 'src/index.ts',
+        output: { file: 'es/agsm-store.mjs', format: 'es', indent: false },
         plugins: [
+            nodeResolve(),
             typescript(),
-            terser()
-        ],
-        output: [
-            { file: pkg.main, format: 'es' }
+            terser({
+                compress: {
+                    pure_getters: true,
+                    unsafe: true,
+                    unsafe_comps: true,
+                    warnings: false
+                }
+            })
+        ]
+    },
+
+    // UMD Development
+    {
+        input: 'src/index.ts',
+        output: {
+            file: 'dist/agsm-store.js',
+            format: 'umd',
+            name: 'AGSM Store',
+            indent: false
+        },
+        plugins: [
+            nodeResolve(),
+            typescript()
+        ]
+    },
+
+    // UMD Production
+    {
+        input: 'src/index.ts',
+        output: {
+            file: 'dist/agsm-store.min.js',
+            format: 'umd',
+            name: 'AGSM Store',
+            indent: false
+        },
+        plugins: [
+            nodeResolve(),
+            typescript(),
+            terser({
+                compress: {
+                    pure_getters: true,
+                    unsafe: true,
+                    unsafe_comps: true,
+                    warnings: false
+                }
+            })
         ]
     }
 ]
